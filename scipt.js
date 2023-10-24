b57879f6ba8ea15bcb26ec2663b40a88b1a383e3
maxonofficial <marshalmaxon33@gmail.com>
2023-10-24 10:26:17 +0530

    counting consonant,number and special character feature updated

--- scipt.js
+++ scipt.js
@@ -1,27 +1,32 @@
 function checkVowels(){
-
- let text = document.getElementById("inputText").value;
- let vowelCount = 0;
-
+  let text = document.getElementById("inputText").value;
+  let vowel = 0, consonant = 0, splChar = 0;
   text = text.toLowerCase();
 
   for(let i = 0; i < text.length; i++){
-
     let char = text.charAt(i);
-    if (isVowel(char)){
-        vowelCount++;
+    if(char >= 'a' && char <= 'z'){
+      if(isVowel(char)){
+        vowel++;
+      }
+      else{
+        consonant++;
+      }
+    }
+    else{
+      if(char != ' ')
+        splChar++;
     }
   }
-
-    
+  
   const result = document.getElementById('result');
-  result.textContent = "Total Vowels: " + vowelCount;
 
-
+  const lineBreak = document.createElement("br");
+  result.appendChild(lineBreak);
+  result.innerHTML = `Total Vowels : ${vowel}<br>Total Consonants : ${consonant} <br>Total Special Characters : ${splChar}`;
 }
 
-
 function isVowel(char){
-    const vowels = ["a","e","i","o","u"];
-    return vowels.includes(char);
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  return vowels.includes(char);
 }
